36e830fe7fe75787a8e36474c88f2e85318ecde3
Przemas <Przemas@PrzemekPC>
2015-11-17 14:10:58 +0100

    Change Text

--- HW/src/HWT.java
+++ HW/src/HWT.java
@@ -3,7 +3,7 @@ public class HWT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Dupa Sraka");
+		System.out.println("Hello GitHub");
 	}
 
 }
